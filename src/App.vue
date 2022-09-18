32b756203f223ae3ba595a30d97f49fac5684e7b
pciruelos <pciruelos@yahoo.com>
2022-09-18 23:14:17 +0200

    periemr comit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Primer/>
+  </div>
+</template>
+
+<script>
+import Primer from './components/Primer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Primer
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Primer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+<h1>Hola mundo asd {{ apellido }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrimerC',
+  props: {},
+  data() {
+    return{
+        apellido: 'apellido juan'
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
